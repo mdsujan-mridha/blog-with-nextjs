562da63e980f47a384606b9799da7b05e1459856
mdsujan-mriddha <mdsujan134523@gmail.com>
2025-05-05 01:42:49 +0600

    update user detais

--- src/adminComponents/UserListTable.js
+++ src/adminComponents/UserListTable.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserModal from './UserModal';
 
 const UserListTable = ({ users }) => {
+
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleEditClick = (user) => {
+        setSelectedUser(user);
+        setIsModalOpen(true);
+    }
+
+    const handleDelete = (userId) => {
+        // Handle delete action here
+        console.log('Delete user with ID:', userId);
+    }
+
+
     return (
         <div className="overflow-x-auto bg-gray-900 rounded-xl p-4 shadow-md">
             <table className="w-full table-auto border-collapse text-white">
@@ -31,7 +47,9 @@ const UserListTable = ({ users }) => {
                                 <td className="py-3 px-6 border capitalize">{user?.role || 'User'}</td>
                                 <td className="py-3 px-6 border">{user?.email || 'N/A'}</td>
                                 <td className="py-3 px-6 border text-center space-x-2">
-                                    <button className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-3 rounded">
+                                    <button className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-3 rounded"
+                                        onClick={() => handleEditClick(user)}
+                                    >
                                         Edit
                                     </button>
                                     <button className="bg-red-600 hover:bg-red-700 text-white py-1 px-3 rounded">
@@ -49,6 +67,17 @@ const UserListTable = ({ users }) => {
                     )}
                 </tbody>
             </table>
+            {
+                isModalOpen && selectedUser && (
+                    <UserModal
+                        handleEditClick={handleEditClick}
+                        setIsModalOpen={setIsModalOpen}
+                        selectedUser={selectedUser}
+                        setSelectedUser={setSelectedUser}
+                        
+                    />
+                )
+            }
         </div>
     );
 };
---
+++ src/adminComponents/UserModal.js
@@ -0,0 +1,71 @@
+
+
+import { X } from 'lucide-react';
+import React from 'react';
+
+const UserModal = ({ handleEditClick, setIsModalOpen, selectedUser }) => {
+
+    // console.log(selectedUser, 'selected user in modal')
+
+    return (
+        <>
+            <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
+                <div className="bg-white p-6 rounded-lg w-96 max-w-md shadow-lg min-h-96">
+                    <button
+                        onClick={() => setIsModalOpen(false)}
+                        className="btn btn-sm btn-circle btn-ghost left-2 top-1"
+                    >✕</button>
+                    <div className='mt-24'>
+                        <h2 className="text-2xl font-semibold mb-4 text-center border-b-2">Update user</h2>
+                        <div>
+                            <input
+
+                                type="text"
+                                className="w-full border p-2 rounded mt-1 mb-4"
+                                value={selectedUser?.name || ''}
+                                onChange={(e) =>
+                                    handleEditClick({ ...selectedUser, name: e.target.value })
+                                }
+                                placeholder="Name"
+
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                className="w-full border p-2 rounded mt-1 mb-4"
+                                value={selectedUser?.email || ''}
+                                onChange={(e) =>
+                                    handleEditClick({ ...selectedUser, email: e.target.value })
+                                }
+                                placeholder="Email"
+
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                className="w-full border p-2 rounded mt-1 mb-4"
+                                value={selectedUser?.role || ''}
+                                onChange={(e) =>
+                                    handleEditClick({ ...selectedUser, role: e.target.value })
+                                }
+                                placeholder="Role"
+
+                            />
+                        </div>
+                        <div className="flex justify-center items-center space-x-4 mb-4">
+                            <button
+                                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+                            >
+                                Save Changes
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UserModal;
